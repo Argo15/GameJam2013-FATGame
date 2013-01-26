073365331452bdf05f06c74051689d72f43686a8
Nikita Tkachov <ntkachov@gmail.com>
2013-01-26 00:21:13 -0500

    added images

--- build/js/background.js
+++ build/js/background.js
@@ -19,7 +19,6 @@ var background = (function(){
 		var layer = new Kinetic.Layer();
 
         var staticBackground = new Image();
-        staticBackground.src = "../media/PNGS/sky3.jpg"; 
 
         staticBackground.onload = function() {
             var yoda = new Kinetic.Image({
@@ -31,12 +30,46 @@ var background = (function(){
             });
             
             layer.add(yoda);
-
 		}
+
+        var cloud1Image = new Image();
+        var cloud2Image = new Image();
+        var cloud3Image = new Image();
+        var cloud4Image = new Image();
+        var cloud5Image = new Image();
+
+        function addLayout(cloudImage){
+            var width = 192;
+            var height = 129
+            cloudImage.onLoad = function(){
+                var yoda = new Kinetic.Image({
+                    x: width,
+                    y: height,
+                    image: cloudImage,
+                    height: height,
+                    width: width
+                });
+                layout.add(yoda);
+            }
+        };
+      
+        addLayout(cloud1Image);
+        addLayout(cloud2Image);
+        addLayout(cloud3Image);
+        addLayout(cloud4Image);
+        addLayout(cloud5Image);
+
+        staticBackground.src = "../media/PNGS/sky3.jpg"; 
+        cloud2Image.src = "../media/PNGS/cloud2.png";
+        cloud1Image.src = "../media/PNGS/cloud1.png";
+        cloud3Image.src = "../media/PNGS/cloud3.png";
+        cloud4Image.src = "../media/PNGS/cloud4.png";
+        cloud5Image.src = "../media/PNGS/cloud5.png";
        
         return [layer]; 
 
 	}
+
 	
 	function setStage(mainStage){
 			stage = mainStage;
