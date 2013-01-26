aa180c456b5497305b6c2d38933899f2dd150805
Bill <argo_15@hotmail.com>
2013-01-26 01:37:46 -0500

    added game states

---
+++ build/js/menustate.js
@@ -0,0 +1,65 @@
+var menustate = (function()
+{
+	var stage;
+	var backgroundLayer,
+		buttonLayer;
+	
+	function init()
+	{
+	    createStage();
+	    addBackground();
+	    addButtons();
+	}
+	
+	function createStage(){
+		stage = new Kinetic.Stage({
+			container: 'container',
+			width: 1280,
+			height: 720,
+		});
+
+		backgroundLayer = new Kinetic.Layer();
+		buttonLayer = new Kinetic.Layer();
+	}
+	
+	function addBackground()
+	{
+	    var backgroundObject = new Kinetic.Rect({
+	      	x: 0,
+	        y: 0,
+	        width: 1280,
+	        height: 720,
+	        fill: 'purple',
+	      })
+	      backgroundLayer.add(backgroundObject);
+	      stage.add(backgroundLayer);
+	}
+	
+	function addButtons()
+	{
+	    var startButton = new Kinetic.Rect({
+	      	x: 540,
+	        y: 510,
+	        width: 200,
+	        height: 100,
+	        fill: 'yellow',
+	      })
+	    buttonLayer.add(startButton);
+	    buttonLayer.on('click', function() {
+            stage.remove();
+            currentstate = gamestate;
+		    currentstate.init();
+        });
+	    stage.add(buttonLayer);
+	}
+
+	function update()
+	{
+	    stage.draw();
+	}
+
+	return{
+		init:init,
+		update:update
+	}
+})();
